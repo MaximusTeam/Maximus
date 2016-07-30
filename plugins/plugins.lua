8ce51c6b102fe5116d9b4d2c6a743d56ad7a08ab
Arian <AllwenWaker81@gmail.com>
2016-07-30 18:57:16 +0430

    Update plugins.lua

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -1,5 +1,5 @@
 local action = function(msg, matches)
-  local text = "🔗 *List Of All Plugins*\n1- _start_\n2- _plugins_\n3- _stats_"
+  local text = "🔗 *List Of All Plugins*\n1- _start_\n2- _plugins_\n3- _stats_\n4- _about_"
   api.sendReply(msg,text,true)
   end
 local triggers = {
